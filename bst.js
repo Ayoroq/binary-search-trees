b92b06e00d21dcd569f750c2ea86b354b1697ab6
Ayo <Ayoroq@gmail.com>
2025-09-05 21:46:17 -0400

    Added the tree class

--- bst.js
+++ bst.js
@@ -1,7 +1,16 @@
+// This is the class that builds the nodes
 class Node{
     constructor(val){
         this.val = val;
         this.left = null;
         this.right  = null;
     }
+}
+
+// This is the tree class
+class Tree{
+    constructor(arr){
+        this.root = this.buildTree(arr);
+    }
+
 }
